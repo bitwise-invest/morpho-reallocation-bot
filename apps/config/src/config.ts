6fb75a548eb7be210e40815dc2a1f4323dfca135
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-12-19 12:23:45 +0100

    feat: replace ponder by blue api

--- apps/config/src/config.ts
+++ apps/config/src/config.ts
@@ -1,24 +1,3 @@
-import { base } from "viem/chains";
+import { base, Chain, mainnet } from "viem/chains";
 
-import type { Config } from "./types";
-
-export const chainConfigs: Record<number, Config> = {
-  [base.id]: {
-    chain: base,
-    morpho: {
-      address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
-      startBlock: 13977148,
-    },
-    adaptiveCurveIrm: {
-      address: "0x46415998764C29aB2a25CbeA6254146D50D22687",
-      startBlock: 13977152,
-    },
-    metaMorphoFactories: {
-      addresses: [
-        "0xFf62A7c278C62eD665133147129245053Bbf5918",
-        "0xA9c3D3a366466Fa809d1Ae982Fb2c46E5fC41101",
-      ],
-      startBlock: 13978134,
-    },
-  },
-};
+export const chains: Chain[] = [mainnet, base];
